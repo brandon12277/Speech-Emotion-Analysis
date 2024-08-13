553611775a883535b010c05f5867f0dc4e5f0ed3
unknown <williamsbrand879@yahoo.com>
2024-08-13 19:05:14 +0530

    updates

--- processing_api/app.py
+++ processing_api/app.py
@@ -23,6 +23,7 @@ app = Flask(__name__)
 CORS(app, origins="*", supports_credentials=True)
 
 
+model = tf.keras.models.load_model("emotion_80.h5")
 
 os.makedirs("uploads", exist_ok=True)
 
@@ -98,17 +99,44 @@ def upload_file():
 
     os.remove(url)
     os.remove(save_path)
-    response = jsonify({'data': resized_spectrogram_image.tolist()})
+
+    result =  model.predict(resized_spectrogram_image)
+    print(result)
+    class_label = 0
+    if result[0][0] == 1:
+        class_label = 0
+
+    elif result[0][1] == 1:
+        class_label = 1
+    elif result[0][2] == 1:
+        class_label = 2
+    elif result[0][3] == 1:
+        class_label = 3
+    elif result[0][4] == 1:
+        class_label = 4
+    elif result[0][5] == 1:
+        class_label = 5
+    elif result[0][6] == 1:
+        class_label = 6
+    elif result[0][7] == 1:
+        class_label = 7
+    
+
+   
+    response = jsonify({'data': class_label})
 
     
     response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
 
     return response  
+   
+
+    
             
 thread = threading.Thread(target=upload_file)
 thread.start()
 thread.join() 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5002,debug=True)
